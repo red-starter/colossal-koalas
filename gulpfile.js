5ef92cd12eff56f2fab395ebc45432b64b561105
camhux <camzig@gmail.com>
2015-09-24 16:46:04 -0700

    Set up linting with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var jscs = require('gulp-jscs');
 
-gulp.task("default", function() {
-
+gulp.task('default', function() {
+  return gulp.src(['./client/**/*.js', './server/**/*.js'])
+    .pipe(jscs())
+    .pipe(jscs.reporter())
+    .pipe(jscs.reporter('fail'));
 });
