42f8a3b7ceba8649b86750d93a69f7c00c7cb5f9
camhux <camzig@gmail.com>
2015-09-25 15:41:16 -0700

    Add TODO, change column name

--- server/routes.js
+++ server/routes.js
@@ -13,10 +13,10 @@ var api = {
     post: function(req, res,cb) {
       console.log('request', req);
       models.User.findOrCreate({
-        where: {username: req.body.username}
+        where: {name: req.body.username}
       }).then(function(user) {
         models.User.create({
-          username: req.body.username,
+          name: req.body.username,
           password: req.body.password
         })
       }).then(function(user) {
