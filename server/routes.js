521105879cd08581cfee7cb39e9608e2a676a117
camhux <camzig@gmail.com>
2015-09-28 16:00:55 -0700

    Mount /api/users POST handler straight to router; rewrite iterative mounting of handlers

--- server/routes.js
+++ server/routes.js
@@ -1,42 +1,8 @@
 var db = require('./database/interface')
-var _ = require('underscore');
 var router = require('express').Router();
 
-var endpoints = {
-
-  '/': {
-
-    /* Potentially unneeded
-    get: function(req, res) {
-      db.User.findAll().then(function(users) {
-        res.json(users);
-      });
-    },
-    */
-
-    post: function(req, res, cb) {
-      // Check to see if a user with this name already exists.
-      db.User.find( {where: { name: req.body.username }} )
-        .then(function(user) {
-          // If a user with this name is found, reject the post request.
-          if (user) {
-            res.status(409).send('User already exists');
-          } else {
-            // Else, we initiate creating the user and pass the promise out to the chain.
-            return db.User.create({ name: req.body.username, password: req.body.password })
-          }
-        })
-        .then(function(user) {
-          // The next branch of the chain will fulfill with the user.
-          // We just send a 201.
-          res.status(201).end();
-        })
-        .catch(function(err) {
-          console.error(err);
-          res.status(500).end();     
-        });
-    }
-  },
+// All of these endpoints will be mounted onto `/api/users`
+var pathHandlers = {
 
   '/:username/entries': {
 
@@ -115,15 +81,41 @@ var endpoints = {
 
 };
 
-// TODO: make a little less clever
-_.each(endpoints, function(handlers, path) {
+// Mount the post to /api/users separately, since it
+// can't be keyed by a path.
+router.post(function(req, res, cb) {
+      // Check to see if a user with this name already exists.
+      db.User.find( {where: { name: req.body.username }} )
+        .then(function(user) {
+          // If a user with this name is found, reject the post request.
+          if (user) {
+            res.status(409).send('User already exists');
+          } else {
+            // Else, we initiate creating the user and pass the promise out to the chain.
+            return db.User.create({ name: req.body.username, password: req.body.password })
+          }
+        })
+        .then(function(user) {
+          // The next branch of the chain will fulfill with the user.
+          // We just send a 201.
+          res.status(201).end();
+        })
+        .catch(function(err) {
+          console.error(err);
+          res.status(500).end();     
+        });
+    });
 
-  var endpoint = router.route(path);
+var path, routePath, method;
 
-  for (var method in handlers) {
-    endpoint[method] = handlers[method];
+for (path in pathHandlers) {
+
+  routePath = router.route(path);
+
+  for (method in path) {
+    routePath[method]( path[method] );
   }
 
-});
+}
 
 module.exports = router;
