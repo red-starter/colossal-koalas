de54a9ab1c087a7fedcf795dcfcfb605c5ff6a89
camhux <camzig@gmail.com>
2015-09-28 13:32:40 -0700

    Begin refactoring API with new database interface

--- server/routes.js
+++ server/routes.js
@@ -1,72 +1,103 @@
-// TODO: use interface here instead of requiring models directly
-var models = require('./database/models');
+var db = require('./database/interface')
 var _ = require('underscore');
 var router = require('express').Router();
 
 var api = {
-  users: {
+
+  '/': {
+
+    /* Potentially unneeded
     get: function(req, res) {
-      models.User.findAll().then(function(users) {
+      db.User.findAll().then(function(users) {
         res.json(users);
       });
     },
-    post: function(req, res, cb) {
-      console.log('request', req);
-      models.User.findOrCreate({
-        where: {name: req.body.username}
-      }).then(function(user) {
-        models.User.create({
-          name: req.body.username,
-          password: req.body.password
-        });
-      }).then(function(user) {
-        console.log('User created: ', user);
-        res.sendStatus(201);
-      });
-    }
-  },
-  prompts: {
-    get: function(req, res) {
-      models.Prompt.findAll().then(function(posts) {
-        res.json(posts);
-      });
-    },
+    */
 
-    post: function(req, res) {
-      models.Prompt.create({
-        text: req.body.text
-      }).then(function(prompt) {
-        console.log('Prompt created: ', prompt);
-        res.sendStatus(201);
-      });
+    post: function(req, res, cb) {
+      // Check to see if a user with this name already exists.
+      db.User.find( {where: { name: req.body.username }} )
+        .then(function(user) {
+          // If a user with this name is found, reject the post request.
+          if (user) {
+            res.status(409).send('User already exists');
+          } else {
+            // Else, we initiate creating the user and pass the promise out to the chain.
+            return db.User.create({ name: req.body.username, password: req.body.password })
+          }
+        })
+        .then(function(user) {
+          // The next branch of the chain will fulfill with the user.
+          // We just send a 201.
+          res.status(201).end();
+        });
     }
   },
-  entries: {
+
+  '/:username/entries': {
+
     get: function(req, res) {
-      models.Entry.findAll().then(function(entries) {
+      db.Entry.findAll({
+
+        include: [{
+          model: 'User', 
+          as: 'users'
+        }],
+
+        where: {
+          'users.name': req.params.username
+        },
+
+        order: [['createdAt', 'DESC']]
+        
+      })
+      .then(function(entries) {
         res.json(entries);
       });
     },
 
     post: function(req, res) {
-      models.Entry.create({
+      db.Entry.create({
         emotion: req.body.emotion,
         text: req.body.text
       }).then(function(entry) {
-        console.log('Entry created: ', entry);
-        res.sendStatus(201);
+        entry.setUser(User.findOne({
+          where: {name: req.params.username}
+        }));
+        res.status(201).json(entry);
       });
     }
+  },
+
+  '/:username/entries/:entryid': {
+
+    get: function(req, res) {
+      db.Entry.findOne({ where: { id: req.params.entryid } })
+        .then(function(entry) {
+          if (!entry) {
+            res.status(404).send();
+          } else {
+            res.json(entry);
+          }
+        });
+    },
+
+    put: function(req, res) {
+
+    },
+
+    delete: function(req, res) {
+
+    }
   }
+
 };
+
+// TODO: make a little less clever
 _.each(api, function(route, key) {
   router.route('/' + key)
-  .get(route.get)
-  .post(route.post);
+    .get(route.get)
+    .post(route.post);
 });
 
 module.exports = router;
-
-
-
-
