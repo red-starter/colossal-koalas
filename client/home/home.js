187d161c1dca6f98bcdf49c907da52acef3c72ac
Cynthia Chen <checy16@gmail.com>
2015-09-30 15:03:32 -0700

    "Modify controller functions to show initial prompt and second prompt after emoji has been selected"

--- client/home/home.js
+++ client/home/home.js
@@ -2,10 +2,13 @@ var home = angular.module('greenfeels.home', []);
 
 home.controller('HomeController', ['$scope', '$state', 'Prompts', 'Entries',
   function($scope, $state, Prompts, Entries) {
+
+    $state.transitionTo('home.initial');
     // Upon load, initialize first prompt. Display of this prompt will depend on the state.
     // Cache the initial prompt in case we want to return to this state.
-    var initPrompt = Prompts.getFirstPrompt();
-    $scope.prompt = initPrompt;
+    // var initPrompt = Prompts.getFirstPrompt();
+    var initPrompt = 'How are you feeling today?';
+    $scope.initialPrompt = initPrompt;
 
     // Initialize a blank model for the entry.
     $scope.entry = {};
@@ -14,9 +17,12 @@ home.controller('HomeController', ['$scope', '$state', 'Prompts', 'Entries',
     // and captures the code of the selected emotion on the tentative entry model.
     $scope.selectHandler = function($event) {
       $state.transitionTo('home.selected');
-      $scope.prompt = Prompts.getSecondPrompt();
+      var secondPrompt = 'Why are you feeling that way?';
+      $scope.secondPrompt = secondPrompt;
+      // $scope.prompt = Prompts.getSecondPrompt();
       $scope.entry.emotion = $event.target.attributes['data-emotion-id'].value;
       console.log('emotion', $scope.entry.emotion);
+      console.log('prompt', $scope.secondPrompt);
     };
 
     $scope.submit = function() {
