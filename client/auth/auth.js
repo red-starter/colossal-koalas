e8ed69dd13f79ebfd324d8826812f67c3c8ad061
Cynthia Chen <checy16@gmail.com>
2015-10-05 12:19:47 -0700

    "Add spinner to signin and signup functions in Auth controller"

--- client/auth/auth.js
+++ client/auth/auth.js
@@ -1,42 +1,55 @@
 angular.module('greenfeels.auth', [])
 
-.controller('AuthController', ['$scope', '$window', '$state', 'Auth',
-  function($scope, $window, $state, Auth) {
+.controller('AuthController', ['$scope', '$window', '$state', 'Auth', 'Spinner',
+  function($scope, $window, $state, Auth, Spinner) {
+
     $scope.user = {};
     $scope.name = $window.localStorage.getItem('moodlet.username');
     $scope.signin = function() {
+      //starts spinner
+      var spinner = Spinner.create();
+      spinner.spin(document.querySelector('.spinner'));
       // calls sign in function from Auth factory to send POST request to api/users
       // receives token from server and stores in local storage
       // transitions state to home
       Auth.signin($scope.user)
-      .then(function (token) {
-        // Store session token for access to secured endpoints
-        $window.localStorage.setItem('moodlet', token);
-        // Store plaintext username for use as a URL parameter in ajax requests
-        $window.localStorage.setItem('moodlet.username', $scope.user.username);
-        $state.transitionTo('home');
-      })
-      .catch(function(error) {
-        console.error(error);
-        $scope.error = error.data;
-      });
-    };
+        .then(function (token) {
+          //stops spinner
+          spinner.stop();
+          // Store session token for access to secured endpoints
+          $window.localStorage.setItem('moodlet', token);
+          // Store plaintext username for use as a URL parameter in ajax requests
+          $window.localStorage.setItem('moodlet.username', $scope.user.username);
+          $state.transitionTo('home');
+        })
+        .catch(function(error) {
+          spinner.stop();
+          console.error(error);
+          $scope.error = error.data;
+        });
+      };
 
     $scope.signup = function() {
+      //starts spinner
+      var spinner = Spinner.create();
+      spinner.spin(document.querySelector('.spinner'));
       // calls sign up function from Auth factory to send POST request to api/users
       // receives token from server and stores in local storage
       // transitions state to home
       Auth.signup($scope.user)
-      .then(function(token) {
-        // Same actions as signin
-        $window.localStorage.setItem('moodlet', token);
-        $window.localStorage.setItem('moodlet.username', $scope.user.username);
-        $state.transitionTo('home');
-      })
-      .catch(function(error) {
-        console.error(error);
-        $scope.error = error.data;
-      });
+        .then(function(token) {
+          //stops spinner
+            spinner.stop();
+          // Same actions as signin
+          $window.localStorage.setItem('moodlet', token);
+          $window.localStorage.setItem('moodlet.username', $scope.user.username);
+          $state.transitionTo('home');
+        })
+        .catch(function(error) {
+          spinner.stop();
+          console.error(error);
+          $scope.error = error.data;
+        });
     };
 
     $scope.signout = function() {
